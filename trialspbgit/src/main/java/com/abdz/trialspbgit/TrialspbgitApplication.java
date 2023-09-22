321bd3b811809f6f2a41c31ade3d306eca457c5c
abdz-Inc <abdul.a.rahman.off@gmail.com>
2023-09-22 19:05:35 +0530

    initial commit

---
+++ trialspbgit/src/main/java/com/abdz/trialspbgit/TrialspbgitApplication.java
@@ -0,0 +1,13 @@
+package com.abdz.trialspbgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialspbgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialspbgitApplication.class, args);
+	}
+
+}
---
+++ trialspbgit/src/test/java/com/abdz/trialspbgit/TrialspbgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdz.trialspbgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialspbgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
